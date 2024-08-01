359ddf177fa082c673088b4d985d39dc68b49b75
C S Vaishakh <vaishakhsubramonian@gmail.com>
2024-08-01 10:11:06 +0530

    commit

--- C/selectionsort.c
+++ C/selectionsort.c
@@ -8,7 +8,7 @@ int main(){
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++){
+    for(i=0;i<n-1;i++){
         min = i;
         for(j=0;j<n-1;j++){
             if(a[j]>a[j+1]){
